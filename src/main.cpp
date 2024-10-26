7b3e1acc6fd4fe2788df22d56c7c3a4fe58b331b
Arthur Brito <arthurbrito@alu.ufc.br>
2024-10-26 14:45:51 -0300

    adicionando grid

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <raylib.h>
+#include "grid.h"
 
 // usaremos a biblioteca raylib para os graficos
 
@@ -7,9 +8,16 @@ int main() {
     InitWindow(300, 600, "Tetris");      // inicializando a janela com 300x600 e o titulo tetris
     SetTargetFPS(60);                    // setando o fps padrão da janela para 60
 
+    Grid grid = Grid();                  // criando a grid criada no header Grid
+    grid.grid[0][0] = 1;
+    grid.grid[3][5] = 4;
+    grid.grid[18][19] = 7;
+    grid.Print();
+
     while(WindowShouldClose() == false) { // loop para, enquanto esc não for pressionado ou o botão de fechar não for pressionado, a janela continuará funcionando
         BeginDrawing();
         ClearBackground(darkBlue);
+        grid.Draw();
 
         EndDrawing();
     };
